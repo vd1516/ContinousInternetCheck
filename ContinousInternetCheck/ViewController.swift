21a9c83bb299af0d8075717b7f54a5ec39362cdf
Dhameliya Vrushabh <vrushabh.dhameliya1516@gmail.com>
2020-12-30 17:50:53 +0530

    Initial Commit

---
+++ ContinousInternetCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContinousInternetCheck
+//
+//  Created by Vrushabh Dhameliya on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
